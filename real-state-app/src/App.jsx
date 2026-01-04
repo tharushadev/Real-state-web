03f141838a7ab59e1fede49ac797a3790e771040
Tharusha Fernando <wtpf2002@gmail.com>
2026-01-04 20:27:19 +0530

    deploy changes

--- real-state-app/src/App.jsx
+++ real-state-app/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { FavouritesProvider } from './context/FavouritesContext';
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
--- real-state-app/vite.config.js
+++ real-state-app/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Real-state-web/',
   server: {
     host: true,
     port: 5173, // Default port or change as needed
